9f56098d15d20fad09f563d42502127990c03ed6
Jordan Rey <rey.jordan.1998@gmail.com>
2025-10-11 15:36:43 +0200

    configure automatic sitemap for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
 // https://astro.build/config
@@ -9,15 +10,30 @@ export default defineConfig({
   // Final website URL, used for sitemap and other SEO purposes
   site: "https://gtnh-test.arenareturns.com",
   output: "static",
-  integrations: [icon({
-    include: {
-      // Only include the icons we need for bundle optimization
-      ic: ["baseline-discord"],
-      mdi: ["github", "arrow-left-bold", "arrow-right-bold", "open-in-new", "check-circle", "alert-circle-outline", "headset", "chevron-down", "calendar", "server", "laptop"],
-      "material-symbols": ["download-rounded", "folder-zip-outline"],
-      tabler: ["scale-outline", "jump-rope", "color-picker", "trophy", "brand-discord"],
-    }
-  })],
+  integrations: [
+    sitemap(),
+    icon({
+      include: {
+        // Only include the icons we need for bundle optimization
+        ic: ["baseline-discord"],
+        mdi: [
+          "github",
+          "arrow-left-bold",
+          "arrow-right-bold",
+          "open-in-new",
+          "check-circle",
+          "alert-circle-outline",
+          "headset",
+          "chevron-down",
+          "calendar",
+          "server",
+          "laptop",
+        ],
+        "material-symbols": ["download-rounded", "folder-zip-outline"],
+        tabler: ["scale-outline", "jump-rope", "color-picker", "trophy", "brand-discord"],
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
